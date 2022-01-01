526010ff6824b843bbf122a430b3bad906b5180f
shazwar <xshazwar@gmail.com>
2022-01-01 17:28:18 +0100

    chore: cleanup old culling artifacts

--- Renderer/Structs.cs
+++ Renderer/Structs.cs
@@ -45,45 +45,5 @@ namespace xshazwar.Renderer
                 planes[i].Normalize();
             }
         }
-
-        public void AssignElement(ref Vector4 v, float x, float y, float z, float w){
-            v.x = x;
-            v.y = y;
-            v.z = z;
-            v.w = w;
-        }
-        public void corners(float tileSize, float height, ref Vector4[] _corners){
-            AssignElement(ref _corners[0], this.x, 0f, this.z, 1f);
-            AssignElement(ref _corners[1], this.x + tileSize, 0f, this.z, 1f);
-            AssignElement(ref _corners[2], this.x + tileSize, 0f, this.z + tileSize, 1f);
-            AssignElement(ref _corners[3], this.x , 0f, this.z + tileSize, 1f);
-
-            AssignElement(ref _corners[4], this.x, height, this.z, 1f);
-            AssignElement(ref _corners[5], this.x + tileSize, height, this.z, 1f);
-            AssignElement(ref _corners[6], this.x + tileSize, height, this.z + tileSize, 1f);
-            AssignElement(ref _corners[7], this.x , height, this.z + tileSize, 1f);
-        }
-
-        public bool visible(Vector3 r){ // relative position
-            if ( r.x < 0f || r.x > 1f ||  r.y < 0f || r.y > 1f || r.z < 0){
-                return false;
-            }
-            return true;
-        }
-        public bool visibleIn(ref Vector4[] planes, float tileSize, float height, ref Vector4[] _corners){
-            this.corners(tileSize, height, ref _corners);
-            int i = 0;
-            foreach(Vector4 p in planes){
-                i = 0;
-                foreach (Vector4 c in _corners){
-                    // if all negative return False
-                    if (Vector4.Dot(p, c) < 0){
-                        i++;
-                    }
-                }
-                if (i == 8){ return false;}
-            }
-            return true;
-        }
     }
 }
